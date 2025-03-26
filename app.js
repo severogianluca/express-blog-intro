715d6bb3ea9e7ec5689bdd85f62b45a36d1a99af
severogianluca <gianlucasevero300@gmail.com>
2025-03-26 17:32:54 +0100

    create: route blog homePage and response server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express ();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log('sei dentro la homepage')
+
+    res.send('server del blog')
+})
+
+app.listen(port, () =>{
+    console.log('il server ti ascolta')
+})
